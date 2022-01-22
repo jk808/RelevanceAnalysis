a965d1e2fa4317ce154be59a747dd5f04c5c2735
Junhyuck Kim <jk808@cam.ac.uk>
2022-01-22 16:19:22 +0000

    chnaging dataset

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import nltk
-from nltk.corpus import movie_reviews
 import random
-
-nltk.download('movie_reviews')
 
 def document_features(document):
     document_words = set(document)
@@ -11,21 +8,33 @@ def document_features(document):
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
+posts = [("Dangerous of escalating climate crisis if we don't take climate action now.", 'rel'), 
+("Trees can live without humans but humans can't servive without trees So please understand the importance of trees and do plantation", 'rel'),
+("Tornadoes Wyoming. What do you think about this? How it happens? Does it happen every Year? How much Tornadoes are dangerous?", 'rel'),
+("Understanding environmental attitudes is vital for addressing many applied environmental problems, ranging from local issues like water pollution to global issues like climate change.", 'rel'),
+("Treat yourself for the new year. All personal training sessios are 20 percent off today only", 'irr'),
+("Text Club members get free 2-day shipping all day on Cyber Monday, no promo code needed. You'll have all of your holiday shopping done and in your hands by Wednesday!", 'irr')
+]
+
+words = []
+for post in posts:
+    words.extend(post[0].split())
+
 if __name__ == '__main__':
-    documents = [(list(movie_reviews.words(fileid)), category)
-                 for category in movie_reviews.categories()
-                 for fileid in movie_reviews.fileids(category)]
+    documents = [(post[0].split(), post[1])
+                 for post in posts]
 
     random.shuffle(documents)
 
     # Define the feature extractor
 
-    all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+    all_words = nltk.FreqDist(w.lower() for w in words)
     word_features = list(all_words)[:2000]
 
     # Train Naive Bayes classifier
     featuresets = [(document_features(d), c) for (d, c) in documents]
-    train_set, test_set = featuresets[100:], featuresets[:100]
+
+    train_set, test_set = featuresets[2:], featuresets[:2]
     classifier = nltk.NaiveBayesClassifier.train(train_set)
 
     # Test the classifier
